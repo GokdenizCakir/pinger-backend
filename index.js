e58d2fd327f60c34b017af68410b514ed1165a9a
GokdenizCakir <gokdenizckr@gmail.com>
2023-09-01 22:23:04 +0300

    fixed async

--- index.js
+++ index.js
@@ -12,32 +12,35 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/', async (req, res) => {
-  try {
-    await axios
-      .get(
-        'https://talep.citroen.com.tr/elektrikli-araclar/app/Home/GetParamsV2?modelCode=astra-e'
-      )
-      .then((response) => {
-        return response.data.reservation_available_color;
-      })
-      .then((data) => {
-        for (let item of data) {
-          if (item.color === 31 && item.available === true) {
-            client.messages
-              .create({
-                body: 'MAVİ RENK AÇILDI',
-                messagingServiceSid: 'MG9ee3b92b47f6a6b10ef53dc1f00b620b',
-                to: '+905322709080',
-              })
-              .then((message) => console.log(message.sid))
-              .catch((err) => console.log(err));
-          }
+  await axios
+    .get(
+      'https://talep.citroen.com.tr/elektrikli-araclar/app/Home/GetParamsV2?modelCode=astra-e'
+    )
+    .then((response) => {
+      return response.data.reservation_available_color;
+    })
+    .then((data) => {
+      for (let item of data) {
+        if (item.color === 31 && item.available === true) {
+          client.messages
+            .create({
+              body: 'MAVİ RENK AÇILDI',
+              messagingServiceSid: 'MG9ee3b92b47f6a6b10ef53dc1f00b620b',
+              to: '+905322709080',
+            })
+            .then((message) => console.log(message.sid))
+            .catch((err) => {
+              console.log(err);
+              return res.status(500).json({ message: 'error' });
+            });
         }
-        res.status(200).json({ message: 'success' });
-      });
-  } catch (err) {
-    console.log(err);
-  }
+      }
+      res.status(200).json({ message: 'success' });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ message: 'error' });
+    });
 });
 
 app.get('/ping', (req, res) => {
