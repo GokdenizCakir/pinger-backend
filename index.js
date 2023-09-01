546fcdef5100a3ab79bb61c8f9b9f39363ac4afd
GokdenizCakir <gokdenizckr@gmail.com>
2023-09-01 22:01:29 +0300

    axios

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { default: axios } = require('axios');
 require('dotenv').config();
 const app = express();
 const client = require('twilio')(
@@ -12,31 +13,36 @@ app.use(cors());
 
 app.get('/', async (req, res) => {
   try {
-    const res = await fetch(
-      'https://talep.citroen.com.tr/elektrikli-araclar/app/Home/GetParamsV2?modelCode=astra-e'
-    );
-    const data = await res.json();
-    for (let item of data.reservation_available_color) {
-      if (item.color === 31) {
-        client.messages
-          .create({
-            body: 'MAVİ RENK AÇILDI',
-            messagingServiceSid: 'MG9ee3b92b47f6a6b10ef53dc1f00b620b',
-            to: '+905322709080',
-          })
-          .then((message) => console.log(message.sid))
-          .catch((err) => console.log(err));
-      }
-    }
+    await axios
+      .get(
+        'https://talep.citroen.com.tr/elektrikli-araclar/app/Home/GetParamsV2?modelCode=astra-e'
+      )
+      .then((response) => {
+        return response.data.reservation_available_color;
+      })
+      .then((data) => {
+        for (let item of data) {
+          if (item.color === 31) {
+            client.messages
+              .create({
+                body: 'MAVİ RENK AÇILDI',
+                messagingServiceSid: 'MG9ee3b92b47f6a6b10ef53dc1f00b620b',
+                to: '+905322709080',
+              })
+              .then((message) => console.log(message.sid))
+              .catch((err) => console.log(err));
+          }
+        }
+      });
   } catch (err) {
     console.log(err);
   }
 });
 
 app.get('/ping', (req, res) => {
-  res.status(200).json({message:'pong'});
+  res.status(200).json({ message: 'pong' });
 });
 
 app.listen(process.env.PORT, () => {
-  console.log('Server listening on port 3000');
+  console.log('Server listening on port 8000');
 });
