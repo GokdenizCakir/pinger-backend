b6708ed1a4873c11b06267f784c464af80d5ddb4
GokdenizCakir <gokdenizckr@gmail.com>
2024-03-10 13:43:23 +0300

    passo

--- index.js
+++ index.js
@@ -1,52 +1,52 @@
 const express = require('express');
 const cors = require('cors');
-const { default: axios } = require('axios');
 require('dotenv').config();
 const app = express();
 const client = require('twilio')(
-  process.env.ACCOUNT_SID,
-  process.env.AUTH_TOKEN
+  'ACe898c8cbe12ebbc107380fb6a626befe',
+  'c0fe4907c64a345d84518efcd87b033d'
 );
 
 app.use(express.json());
 app.use(cors());
 
 app.get('/', async (req, res) => {
-  await axios
-    .get(
-      'https://talep.citroen.com.tr/elektrikli-araclar/app/Home/GetParamsV2?modelCode=astra-e'
-    )
-    .then((response) => {
-      return response.data.reservation_available_color;
-    })
-    .then((data) => {
-      for (let item of data) {
-        if (item.color === 31 && item.available === true) {
-          client.messages
-            .create({
-              body: 'MAVİ RENK AÇILDI',
-              messagingServiceSid: 'MG9ee3b92b47f6a6b10ef53dc1f00b620b',
-              to: '+905322709080',
-            })
-            .then((message) => console.log(message.sid))
-            .catch((err) => {
-              console.log(err);
-              return res.status(500).json({ message: 'error' });
-            });
-        }
-      }
-      res.status(200).json({ message: 'success', data });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({ message: 'error' });
+  // Add an authorization header with the value of the token
+
+  const response = await fetch(
+    'https://ticketingweb.passo.com.tr/api/passoweb/getavailableblocklist?eventId=6231334&serieId=&seatCategoryId=7341461',
+    {
+      body: null,
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization:
+          'Bearer DHZHUMUdA8HUDTlgRWfY+s0KGNYpmPEYGyVinAenPkIPzJlbP6cgwBWoRK15INXUnHSjw70ukEEtE8vhHRlgWLXnWMyVn8gTz5YeteZ+dUzqNsUWxitAaHrh+A9AU7ABA8YL2s/b/bRUeg//9OdGmBnscyuJ0zerXbOzvFRDoss=',
+        'Set-Cookie':
+          'TS014d1c97=01de2447c6bbdeeb298d0fa41e67c0943f19f2f7c90a7791db90c78c3809e442aad295fbb18265f1422740e2e141130ad3f5c8935a; Path=/; Domain=.ticketingweb.passo.com.tr',
+      },
+    }
+  );
+
+  const data = await response.json();
+
+  if (data?.totalItemCount > 0) {
+    const message = await client.messages.create({
+      body: 'FB MAÇI',
+      from: '+1 727 288 9005',
+      to: '+905322709080',
     });
+
+    console.log(message.sid);
+  }
+
+  res.status(200).json(data);
 });
 
 app.get('/ping', (req, res) => {
   res.status(200).json({ message: 'pong' });
 });
 
-app.listen(process.env.PORT, () => {
+app.listen(8000, () => {
   console.log('Server listening on port 8000');
 });
